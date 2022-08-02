ac7a3ef55bba9e767843a3656cd8ad10dec57cb6
Dominic <git@msrd0.de>
2022-08-02 17:36:38 +0200

    implement ellipse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-	println!("Hello, world!");
+use clap::Parser;
+use ellipse::Ellipse as _;
+use std::io::{self, BufRead as _};
+
+#[derive(Parser)]
+#[clap(name = "ellipse")]
+#[clap(about, version)]
+struct Args {
+	/// The number of grapheme clusters after which to truncate.
+	#[clap(short = 'n')]
+	len: usize,
+
+	/// The string used for ellipse.
+	#[clap(short, default_value = "…")]
+	ellipsis: String
+}
+
+fn main() -> io::Result<()> {
+	let args = Args::parse();
+	let stdin = io::stdin();
+	for line in stdin.lock().lines() {
+		println!(
+			"{}",
+			line?
+				.as_str()
+				.truncate_ellipse_with(args.len, &args.ellipsis)
+		);
+	}
+	Ok(())
 }
